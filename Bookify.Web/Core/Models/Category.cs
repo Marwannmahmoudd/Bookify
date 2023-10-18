d76972b4e27a1e1dfa7d0d8bc7a3153e0cbeae2a
Marwan Mahmoud <Mwdo49@gmail.com>
2023-10-18 10:49:32 +0300

    Add Category Table

---
+++ Bookify.Web/Core/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Web.Core.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [MaxLength(100), Required]
+        public string Name { get; set; } = null!;
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime? LastUpdatedOn { get; set; }
+    }
+}
