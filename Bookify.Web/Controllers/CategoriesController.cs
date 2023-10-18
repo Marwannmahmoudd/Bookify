6baf4f5888f175c7158069be0d0c49004044c9f6
Marwan Mahmoud <Mwdo49@gmail.com>
2023-10-18 11:25:56 +0300

    add category controller and index view

---
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public IActionResult Index()
+        {
+            //to do : use viewModel
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
