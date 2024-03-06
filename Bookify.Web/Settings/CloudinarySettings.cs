85025e3a39b4cda9f69bc8c97544b46c981658fe
Marwan Mahmoud <Mwdo49@gmail.com>
2024-03-06 17:32:39 +0200

    Add project files.

---
+++ Bookify.Web/Settings/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Web.Settings
+{
+    public class CloudinarySetting
+    {
+        public string Cloud { get; set; } = null!;
+        public string ApiKey { get; set; } = null!;
+        public string ApiSecret { get; set; } = null!;
+    }
+}
